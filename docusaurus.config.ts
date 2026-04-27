c03adae38205a60709d5e432541346ea32cbebbd
Vivian Wang <wangruikang@iscas.ac.cn>
2026-04-27 14:39:53 +0800

    Configure CommonMark settings
    Parse all .md files as CommonMark, and also disable
    markdown.mdx1Compat.headingIds. Without these, the MDX 1 compatibility
    code in Docusaurus messes up code blocks containing text like {#foo}.
    
    Signed-off-by: Vivian Wang <wangruikang@iscas.ac.cn>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -173,6 +173,13 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+
+  markdown: {
+    format: 'detect',
+    mdx1Compat: {
+      headingIds: false
+    },
+  },
 };
 
 export default config;
